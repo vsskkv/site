7aa7b97d2f790c60e1b067c851f4d50e6b3c5191
vikram <vsskkv@gmail.com>
2019-06-21 10:39:29 +0100

    Added background to welcome page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="{{ URL::asset('css/index.css') }}" />
+  </head>
+  <body>
+    <div id="parallax-container">
+        <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/SkyBG.png);"></div>
+        <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/Clouds1.png);"></div>
+        <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/Clouds2.png);"></div>
+        <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/Clouds3.png);"></div>
+        <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/Moon.png)"></div>
+        <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/Hill.png)"></div>
+    </div>
+    <div id="content">
+        <div style="height: 2000px;">
+        <!--Content-->
+        </div>
+    </div>
+
+    <script type="text/javascript" src="{{ URL::asset('js/index.js') }}"></script>
+  </body>
+</html>
+
