88f412e01297b1e4b6334fc39b02d3c52c14ed37
vikram <vsskkv@gmail.com>
2019-07-04 21:14:31 +0100

    RESTART
    -sometimes the best plan is to restart

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,42 +4,23 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Vikrams Site</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{ URL::asset('css/index.css') }}" />
     </head>
     <body>
-
-        <div id="parallax-container">
-            <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/SkyBG.png);"></div>
-            <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/Clouds1.png);"></div>
-            <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/Clouds2.png);"></div>
-            <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/Clouds3.png);"></div>
-            <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/Moon.png)"></div>
-            <div style="background: url(https://cdn2.hubspot.net/hubfs/1951013/Parallax/Hill.png)"></div>
-
-            <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/sign') }}">sign-up/Sign-in</a>
-                    @endauth
-                </div>
-            @endif
-        </div>
-
-        </div>
-        
-        <div id="content">
-            <div style="height: 2000px;">
-            <!--Content-->
+    <!--Navbar-->
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ url('/sign') }}">sign-up/Sign-in</a>
+                @endauth
             </div>
-        </div>
-
+        @endif
         <script type="text/javascript" src="{{ URL::asset('js/index.js') }}"></script>
     </body>
 </html>
